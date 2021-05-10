16cf9f74cc48d5fbae2b05bd71b9a76d29590b6b
bidok <bidokfeed@gmail.com>
2021-05-10 20:59:28 +0300

    Initial commit

---
+++ src/lab_1.java
@@ -0,0 +1,25 @@
+public class lab_1 {
+    public static void main(String[] args) {
+        String nameTheme = "Star Wars";
+        byte numOfFilms = 9;
+        short numOfBooks = 1300;
+        int firstCashFees = 775498007;
+        float lenghtOfBlade = 10.6299F;
+        boolean truthFact = true;
+        char [] namesOfPerson = {'j','e','d','i'};
+        double numsPersonOfKotusant = 1250 * Math.pow(10,9) ;
+
+       /* System.out.println("My theme is " + nameTheme + " universe. A total of " + numOfFilms + " films were made. " +
+                "Was also written " + numOfBooks + " books. The first film on franchise to ern " + firstCashFees + " dollars. " +
+                "In this world the most strong people is " + namesOfPerson[0] +namesOfPerson[1] + namesOfPerson[2] +
+                namesOfPerson[3] + ". They are use light blade, it`s lenght is "+ lenghtOfBlade + " inch" +
+                ". The main planet in Respublic is Korusant " + "and there are " + numsPersonOfKotusant + " people lives on it." +
+                " All facts in this text is " + truthFact + ".");*/
+
+        String str = "";
+
+
+
+
+    }
+}
