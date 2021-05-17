8b05c73d277737f6dbc1d26b4479ddac239a56ec
Vasyl Bidyak <v.bidyak@asd-team.com>
2021-05-17 09:54:48 +0300

    1

---
+++ src/Person.java
@@ -0,0 +1,21 @@
+/**
+ * @author : bidok
+ * @created : 13.05.2021, четверг
+ * @className : Person
+ **/
+
+public class Person {
+	private Integer age;
+
+	public Person(Integer age) {
+		this.age = age;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+}
---
+++ src/test/test.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author : Vasyl Bidiak
+ * @created : 17.05.2021, понедельник
+ * @className : test.test
+ **/
+
+public class test {
+	String test;
+}
